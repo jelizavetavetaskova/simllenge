feb576571c06edfc03806b0047a82377e83f104f
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-07-31 19:07:09 +0300

    Initial commit

---
+++ src/main/java/dev/vulpden/simllenge/SimllengeApplication.java
@@ -0,0 +1,13 @@
+package dev.vulpden.simllenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimllengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimllengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vulpden/simllenge/SimllengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vulpden.simllenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimllengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
