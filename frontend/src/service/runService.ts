34f255bb0dd67b2c8f8640f22fa9623882e691fc
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-08-06 16:56:07 +0300

    feat: added a page for one run and a link in list of runs

--- frontend/src/service/runService.ts
+++ frontend/src/service/runService.ts
@@ -10,6 +10,15 @@ export const getChallengeRuns = async (challengeId: string) => {
     return data;
 }
 
+export const getRunById = async (challengeId: string, runId: string) => {
+    const res = await fetch(`/api/challenges/${challengeId}/runs/${runId}`);
+
+    if (!res.ok) throw Error(await res.text());
+
+    const data: Run = await res.json();
+    return data;
+}
+
 export const createRun = async (challengeId: string, run: CreateRun) => {
     const res = await fetch(`/api/challenges/${challengeId}/runs`, {
         method: "POST",
