27ce2275690db6df4ce1a148ba76af586c67e7c1
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2026-08-13 15:02:36 +0300

    feat: added controller for login

--- src/main/java/dev/vulpden/simllenge/auth/controller/AuthController.java
+++ src/main/java/dev/vulpden/simllenge/auth/controller/AuthController.java
@@ -1,11 +1,20 @@
 package dev.vulpden.simllenge.auth.controller;
 
+import dev.vulpden.simllenge.auth.dto.LoginDto;
 import dev.vulpden.simllenge.auth.dto.RegisterDto;
 import dev.vulpden.simllenge.auth.service.RegisterService;
 import dev.vulpden.simllenge.user.dto.UserDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final RegisterService registerService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(RegisterService registerService) {
+    public AuthController(RegisterService registerService, AuthenticationManager authenticationManager) {
         this.registerService = registerService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserDto> register(@Valid @RequestBody RegisterDto registerDto) {
         UserDto user = registerService.register(registerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginDto loginDto, HttpServletRequest httpReq) {
+        try {
+            Authentication auth = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            HttpSession session = httpReq.getSession(true);
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+            return ResponseEntity.ok().build();
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or/and password");
+        }
     }
 }
